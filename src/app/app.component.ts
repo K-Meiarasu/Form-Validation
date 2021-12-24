94ae661f3ef87150ef29d8b3be3e1e9553bb2698
Meiarasu K <gauthammeiarasu@gmail.com>
2021-12-24 21:38:24 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+  
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.registerForm=this.formBuilder.group({
+      name:['',[Validators.required, Validators.minLength(8), Validators.maxLength(50)]],
+      // gender:['',[Validators.required]],
+      // country:['',[Validators.required]],
+      city:['',[Validators.required]],
+      age:['',[Validators.required]]
+    });
+  }
+  get fun(){
+    return this.registerForm.controls;
+  }
+  onSubmit(){
+    if(this.registerForm.invalid){
+      alert("Validation failed. Try Again...!");
+      return;
+    }
+    alert("Registration done successfully...!");
+  }
 }
